e18a49b3dfba8a501447e3c1a3940ecfb537f222
alex jiang <alexjiang201507@gmail.com>
2024-12-28 15:19:09 +1100

    Modify starting light color

--- include/PCH.h
+++ include/PCH.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,16 @@
 void InitializeLog()
 {
 	auto path = logger::log_directory();
-	if (!path) {
-		SKSE::stl::report_and_fail("Failed to find standard logging directory"sv);
+	if (!path)
+	{
+		SKSE::stl::report_and_fail(
+			"Failed to find standard logging directory"sv);
 	}
 
 	*path /= Version::PROJECT;
 	*path += ".log"sv;
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+	auto sink =
+		std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 
 	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
 
@@ -24,7 +27,6 @@ void InitializeLog()
 	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
 }
 
-
 #ifdef SKYRIM_AE
 extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
 	SKSE::PluginVersionData v;
@@ -38,13 +40,15 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
 	return v;
 }();
 #else
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+extern "C" DLLEXPORT bool SKSEAPI
+	SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
 	a_info->infoVersion = SKSE::PluginInfo::kVersion;
-	a_info->name = "CineStudio";
-	a_info->version = Version::MAJOR;
+	a_info->name        = "CineStudio";
+	a_info->version     = Version::MAJOR;
 
-	if (a_skse->IsEditor()) {
+	if (a_skse->IsEditor())
+	{
 		logger::critical("Loaded in editor, marking as incompatible"sv);
 		return false;
 	}
@@ -52,11 +56,12 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 	const auto ver = a_skse->RuntimeVersion();
 	if (ver
 #	ifndef SKYRIMVR
-		< SKSE::RUNTIME_1_5_39
+	    < SKSE::RUNTIME_1_5_39
 #	else
-		> SKSE::RUNTIME_VR_1_4_15_1
+	    > SKSE::RUNTIME_VR_1_4_15_1
 #	endif
-	) {
+	)
+	{
 		logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
 		return false;
 	}
@@ -65,12 +70,29 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 }
 #endif
 
-void UpdateLight(RE::TESObjectLIGH* a_light, const RE::NiPointer<RE::NiPointLight>& a_ptLight, RE::TESObjectREFR* a_ref, float a_wantDimmer)
+namespace RE
 {
-	using func_t = decltype(&UpdateLight);
-	static REL::Relocation<func_t> func{ RELOCATION_ID(17212, 17614) };
-	func(a_light, a_ptLight, a_ref, a_wantDimmer);
-}
+	template <class T>
+	void UpdateNode(T* a_obj)
+	{
+		if (TaskQueueInterface::ShouldUseTaskQueue())
+		{
+			QueueUpdateNiObject(TaskQueueInterface::GetSingleton(), a_obj);
+		}
+		else
+		{
+			NiUpdateData updateData;
+			a_obj->Update(updateData);
+		}
+	}
+
+	void QueueUpdateNiObject(TaskQueueInterface* self, NiAVObject* a_obj)
+	{
+		using func_t = decltype(QueueUpdateNiObject);
+		static REL::Relocation<func_t> func{ RELOCATION_ID(35929, 36904) };
+		return func(self, a_obj);
+	}
+}  // namespace RE
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
@@ -79,22 +101,36 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
 
 	logger::info("Game version : {}", a_skse->RuntimeVersion().string());
 
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message) {
-		if (message->type == SKSE::MessagingInterface::kDataLoaded) {
-			RE::ConsoleLog::GetSingleton()->Print("CineStudio has been loaded");
-		}
-		if (message->type == SKSE::MessagingInterface::kPostLoadGame) {
-			// ALTERNATE START TORCH REFID 0x22073cac
-			auto*                                  torch = RE::TESForm::LookupByID(0xff000d4e)->As<RE::TESObjectREFR>();
-			static RE::NiPointer<RE::NiPointLight> pointLight(RE::NiPointLight::Create());
-			auto*                                  baseLight = reinterpret_cast<RE::TESObjectLIGH*>(torch->GetBaseObject());
-			pointLight->diffuse.red = 0;
-			pointLight->diffuse.blue = 255;
-			pointLight->diffuse.green = 0;
-			pointLight->radius = RE::NiPoint3(500.0f,500.0f,500.0f);
-			UpdateLight(baseLight, pointLight, torch, false);
-		}
-	});
+	SKSE::GetMessagingInterface()->RegisterListener(
+		[](SKSE::MessagingInterface::Message* message) {
+			if (message->type == SKSE::MessagingInterface::kDataLoaded)
+			{
+				RE::ConsoleLog::GetSingleton()->Print("CineStudio has been loaded");
+			}
+			if (message->type == SKSE::MessagingInterface::kPostLoadGame)
+			{
+				// ALTERNATE START TORCH REFID 0x22173cad
+				// ff000d4e
+				auto* lightObjRef = RE::TESForm::LookupByID(0x22173cad)->As<RE::TESObjectREFR>();
+
+				auto niRoot = lightObjRef->Get3D()->AsFadeNode();
+
+				for (const auto& a : niRoot->children)
+				{
+					logger::info("{}", a->name.c_str());
+				}
+
+				auto niAvNode = niRoot->children[0].get();
+
+				auto* light = skyrim_cast<RE::NiLight*>(niAvNode);
+
+				light->diffuse.red   = 0;
+				light->diffuse.blue  = 0.5f;
+				light->diffuse.green = 0;
+
+				// RE::UpdateNode(niAvNode);
+			}
+		});
 
 	return true;
 }
